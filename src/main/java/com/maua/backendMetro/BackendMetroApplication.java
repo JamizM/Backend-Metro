f54a8429baa96fcf53f14fe3d7667268ce7623cd
VictorCodinhoto <133376215+VictorCodinhoto@users.noreply.github.com>
2024-08-27 14:49:41 -0300

    Adding Spring Files

---
+++ src/main/java/com/maua/backendMetro/BackendMetroApplication.java
@@ -0,0 +1,13 @@
+package com.maua.backendMetro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMetroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMetroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maua/backendMetro/BackendMetroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maua.backendMetro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
