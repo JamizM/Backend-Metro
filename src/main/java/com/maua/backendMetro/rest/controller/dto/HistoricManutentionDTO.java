e365dcea1cfbc8e145a3e2a347c10d1fe4248626
João Antônnio Martinez <133376282+JamizM@users.noreply.github.com>
2024-10-10 18:08:32 -0300

    creating historic DTO and resolving vulnerability problem on pom.xml

--- src/main/java/com/maua/backendMetro/rest/controller/dto/HistoricManutentionDTO.java
+++ src/main/java/com/maua/backendMetro/rest/controller/dto/HistoricManutentionDTO.java
@@ -1,18 +1,21 @@
 package com.maua.backendMetro.rest.controller.dto;
 
+import com.maua.backendMetro.domain.entity.Extinguisher;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class HistoricManutentionDTO {
-    private Long idManutention;
-    private String maintenanceData;
+    private Long id;
+    private LocalDate maintenanceData;
     private String description;
     private String responsible;
     @NotNull(message = "Extinguisher ID is required")
-    private String extinguisherId;
+    private String extinguisher;
 }
