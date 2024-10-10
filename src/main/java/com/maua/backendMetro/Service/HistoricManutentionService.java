879fc2b4ae97072d7c7d336a1deb8f1df4c03467
João Antônnio Martinez <133376282+JamizM@users.noreply.github.com>
2024-10-10 18:07:30 -0300

    Adding service and implementation

--- src/main/java/com/maua/backendMetro/Service/HistoricManutentionService.java
+++ src/main/java/com/maua/backendMetro/Service/HistoricManutentionService.java
@@ -5,5 +5,5 @@ import com.maua.backendMetro.rest.controller.dto.HistoricManutentionDTO;
 
 public interface HistoricManutentionService {
 
-    public HistoricManutention createHistoricManutention(HistoricManutentionDTO historicManutentionDTO);
+    public HistoricManutention createHistoricManutention(HistoricManutentionDTO dto);
 }
--- src/main/java/com/maua/backendMetro/Service/Impl/HistoricManutentionImpl.java
+++ src/main/java/com/maua/backendMetro/Service/Impl/HistoricManutentionImpl.java
@@ -3,9 +3,9 @@ package com.maua.backendMetro.Service.Impl;
 import com.maua.backendMetro.Service.HistoricManutentionService;
 import com.maua.backendMetro.domain.entity.Extinguisher;
 import com.maua.backendMetro.domain.entity.HistoricManutention;
+import com.maua.backendMetro.domain.repository.Extinguishers;
 import com.maua.backendMetro.domain.repository.HistoricManutentions;
 import com.maua.backendMetro.rest.controller.dto.HistoricManutentionDTO;
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,23 +17,21 @@ import java.time.LocalDate;
 public class HistoricManutentionImpl implements HistoricManutentionService {
 
     @Autowired
-    private EntityManager entityManager;
-
-    @Autowired
     private HistoricManutentions historicManutentions;
 
+    @Autowired
+    private Extinguishers extinguishers;
+
     public HistoricManutention createHistoricManutention(HistoricManutentionDTO dto) {
-        Extinguisher extinguisher = entityManager.getReference(Extinguisher.class, dto);
+        Extinguisher extinguisher = extinguishers.findById(dto.getExtinguisher())
+                .orElseThrow(() -> new RuntimeException("Extinguisher not found with ID: " + dto.getExtinguisher()));
 
         HistoricManutention historicManutention = new HistoricManutention();
-
+        historicManutention.setMaintenanceData(dto.getMaintenanceData());
+        historicManutention.setDescription(dto.getDescription());
+        historicManutention.setResponsible(dto.getResponsible());
         historicManutention.setExtinguisher(extinguisher);
 
-        historicManutention.setMaintenanceData(LocalDate.now());
-        historicManutention.setDescription("Manutenção realizada");
-        historicManutention.setResponsible("João da Silva");
-
-        historicManutentions.save(historicManutention);
-        return historicManutention;
+        return historicManutentions.save(historicManutention);
     }
 }
