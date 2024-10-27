e6301cf3c3b963b9131dfc13e3c3940769a279d4
João Antônnio Martinez <joao.antonnio.m2005@gmail.com>
2024-10-27 15:43:40 -0300

    Configuring tests and doing findExtinguisherByLocalization_AreaAndLocalization_SubwayStationAndLocalization_DetailedLocation_Success test

--- src/test/java/com/maua/backendMetro/domain/repository/ExtinguishersTest.java
+++ src/test/java/com/maua/backendMetro/domain/repository/ExtinguishersTest.java
@@ -1,12 +1,19 @@
 package com.maua.backendMetro.domain.repository;
 
 import com.maua.backendMetro.domain.entity.Extinguisher;
+import com.maua.backendMetro.domain.entity.Localization;
+import com.maua.backendMetro.domain.entity.enums.ExtinguisherStatus;
+import com.maua.backendMetro.domain.entity.enums.ExtinguisherType;
 import com.maua.backendMetro.rest.controller.dto.ExtinguisherDTO;
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,8 +24,59 @@ class ExtinguishersTest {
     @Autowired
     EntityManager entityManager;
 
-    @Test
-    void findExtinguisherByLocalization() {
+    @Autowired
+    Extinguishers extinguishers;
 
+    @Autowired
+    Localizations localizations;
+
+    //testes do repository
+    private Extinguisher createExtinguisher(Extinguisher extinguisher) {
+        Extinguisher newExtinguisher = new Extinguisher(extinguisher);
+        this.entityManager.persist(newExtinguisher);
+        return newExtinguisher;
+    }
+
+    private Localization createLocalization(Localization localization){
+        Localization newLocalization = new Localization(localization);
+        this.entityManager.persist(newLocalization);
+        return newLocalization;
+    }
+
+    @Test
+    @DisplayName("Should return a extinguisher by localization area, subway station and detailed location")
+    void findExtinguisherByLocalization_AreaAndLocalization_SubwayStationAndLocalization_DetailedLocation_Success() {
+        Localization localization = new Localization("RED_LINE", "JABAQUARA", "Plataforma 1");
+
+        entityManager.persist(localization);
+
+        Extinguisher extinguisher = new Extinguisher("0EX143-4442",
+                "FOAM",
+                10,
+                "GIEFL",
+                "2025-12-01",
+                "2023-01-01",
+                123,
+                "2025-01-01",
+                "OK");
+        extinguisher.setLocalization(localization);
+
+        createExtinguisher(extinguisher);
+
+        Optional<Extinguisher> foundedExtinguisherByLocalization = extinguishers.findExtinguisherByLocalization_AreaAndLocalization_SubwayStationAndLocalization_DetailedLocation(
+                localization.getArea(),
+                localization.getSubwayStation(),
+                localization.getDetailedLocation());
+
+        assertThat(foundedExtinguisherByLocalization.isPresent()).isTrue();
+    }
+
+
+    @Test
+    void findExtinguisherByExtinguisherStatus() {
+    }
+
+    @Test
+    void findExtinguisherByExtinguisherType() {
     }
 }
